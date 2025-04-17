8ea1768e3f7334761b959b60ac42f914071b3469
Abel Garcia <148645860+abel8000000@users.noreply.github.com>
2025-04-17 04:32:30 +0000

    Migrating to React

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="content-box">
+        <h1>Trabajo final</h1>
+        <h3>Telematica, 2025</h3>
+        <p>Pagina para el proyecto final de telematica; alojada en un container de Docker en un EC2 de AWS.</p>
+        <img src="spinning-maxwell.gif" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
