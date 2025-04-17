31c6c9609cf52909bd296bb4dedf3c233aa46461
Abel Garcia <148645860+abel8000000@users.noreply.github.com>
2025-04-17 07:04:35 +0000

    deploy fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,10 @@
 # Navega al directorio de la aplicación
 cd ~/app
 
-# Construye la imagen Docker
+# Construye la imagen Docker#!/bin/bash
+export PATH=$PATH:/usr/bin:/usr/local/bin
 docker build -t final_telematica . || { echo "Docker build failed"; exit 1; }
+
 
 # Detiene y elimina cualquier contenedor en ejecución con el mismo nombre
 docker rm -f final_telematica_container || true
