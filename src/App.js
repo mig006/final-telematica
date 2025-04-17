48fe22156ddd3292acbfcbfabf4235edef780063
Abel Garcia <148645860+abel8000000@users.noreply.github.com>
2025-04-17 05:45:21 +0000

    Jenkins test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
         <h1>Trabajo final</h1>
         <h3>Telematica, 2025</h3>
         <p>Pagina para el proyecto final de telematica; alojada en un container de Docker en un EC2 de AWS.</p>
+        <p>Prueba 1</p>
         <img src="spinning-maxwell.gif" />
       </div>
     </div>
